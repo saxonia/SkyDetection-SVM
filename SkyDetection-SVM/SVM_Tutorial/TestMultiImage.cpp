f0b9b0949d00b8fc00bd71385edb12415354128d
Thiruch Rakthao <sax_inw_com@hotmail.com>
2016-03-17 20:21:46 +0700

    Finished SVM using multiple HSV Image focus on Hue in color space

--- SkyDetection-SVM/SVM_Tutorial/TestMultiImage.cpp
+++ SkyDetection-SVM/SVM_Tutorial/TestMultiImage.cpp
@@ -6,6 +6,14 @@
 using namespace std;
 using namespace cv;
 
+#define dataWidth 600
+#define dataHeight 350
+
+int numFiles = 9;
+const int stIdx = 0;
+const int enIdx = 8;
+
+
 // Mouse Callback function
 void CallBackFunc(int event,int x,int y,int flags,void *userdata)
 {
@@ -21,23 +29,22 @@ void CallBackFunc(int event,int x,int y,int flags,void *userdata)
 int main()
 {
 	//New 
-	int numFiles = 4;
-	int imgArea = 600*350;
+	int imgArea = dataWidth*dataHeight;
 	Mat trainingMat(imgArea*numFiles,1,CV_32FC1);
 	Mat labelsMat(imgArea*numFiles,1,CV_32FC1);
 
 	int ii = 0; // Current row in trainingMat
 	int jj = 0; // Current row in labelsMat
-	for(int k = 0;k < numFiles;k++)
+	for(int k = stIdx;k < enIdx;k++)
 	{
 		stringstream ss;
 		ss << k;
 		String str = ss.str();
 		String imgName = "SVMDataset/";
 		imgName = imgName + str + ".png";
-		cout << imgName << endl;
+		//cout << imgName << endl;
 		Mat inputImg = imread(imgName); // I used 0 for greyscale
-		resize(inputImg,inputImg,Size(600,350));
+		resize(inputImg,inputImg,Size(dataWidth,dataHeight));
 
 		Mat inputHSV;
 		cvtColor(inputImg,inputHSV,CV_BGR2HSV);
@@ -50,18 +57,10 @@ int main()
 			}
 		}
 
-		/*if(k <3)
-		{
-		labelsMat.at<float>(k,0) = 1.0;
-		}
-		else
-		{
-		labelsMat.at<float>(k,0) = -1.0;
-		}*/
 		for(int i = 0;i < imgArea;i++)
 		{
-			bool clause1 = inputHSV.at<Vec3b>(i/inputImg.cols,i%inputImg.cols)[0] > 75;
-			bool clause2 = inputHSV.at<Vec3b>(i/inputImg.cols,i%inputImg.cols)[0] < 120;
+			bool clause1 = inputHSV.at<Vec3b>(i/inputImg.cols,i%inputImg.cols)[0] >= 75;
+			bool clause2 = inputHSV.at<Vec3b>(i/inputImg.cols,i%inputImg.cols)[0] <= 120;
 			if(clause1 && clause2)
 				labelsMat.at<float>(jj++,0) = 1.0;
 			else
@@ -89,17 +88,19 @@ int main()
 	svm.save("fin");
 	svm.load("fin");
 
-	Mat image = imread("1.png",0);
-	resize(image,image,Size(600,350));
-	Mat res = Mat::zeros(Size(image.cols,image.rows),CV_8UC3);
+	Mat testImage = imread("SVMTest/3.png");
+	resize(testImage,testImage,Size(dataWidth,dataHeight));
+	Mat testImageHSV;
+	cvtColor(testImage,testImageHSV,CV_BGR2HSV);
+	Mat res = Mat::zeros(Size(testImage.cols,testImage.rows),CV_8UC3);
 	Vec3b green(0,255,0), blue (255,0,0);
 
-	for (int i = 0; i < image.rows; ++i)
-		for (int j = 0; j < image.cols; ++j)
+	for (int i = 0; i < testImage.rows; ++i)
+		for (int j = 0; j < testImage.cols; ++j)
 		{
 			//Mat sampleMat = (Mat_<float>(1,1) << 1);
 
-			Mat sampleMat = (Mat_<float>(1,1) << image.at<uchar>(i,j));
+			Mat sampleMat = (Mat_<float>(1,1) << testImageHSV.at<Vec3b>(i,j)[0]);
 			float response = svm.predict(sampleMat);
 
 			if (response == 1)
@@ -110,35 +111,33 @@ int main()
 
 	/*Mat testMat(1,imgArea,CV_32FC1); 
 	ii = 0;
-	for(int i = 0;i < image.rows;i++)
+	for(int i = 0;i < testImage.rows;i++)
 	{
-		for (int j = 0; j < image.cols; j++)
+		for (int j = 0; j < testImage.cols; j++)
 		{
-			testMat.at<float>(0,ii++) = image.at<Vec3b>(i,j)[0];
+			testMat.at<float>(0,ii++) = testImage.at<Vec3b>(i,j)[0];
 		}
 	}
 
-	for(int i = 0;i < image.rows;i++)
+	for(int i = 0;i < testImage.rows;i++)
 	{
-		for (int j = 0; j < image.cols; j++)
+		for (int j = 0; j < testImage.cols; j++)
 		{
-			Mat sampleMat = (Mat_<float>(1,1) << image.at<uchar>(i,j));
+			Mat sampleMat = (Mat_<float>(1,1) << testImage.at<uchar>(i,j));
 			float response = svm.predict(testMat);
 		}
 	}*/
-
-
+	imshow("testImage",testImage);
+	//setMouseCallback("testImage",CallBackFunc,&testImageHSV);
 	imshow("res",res);
-	Mat grey = imread("SVMDataset/1.png");
+	/*Mat grey = imread("SVMDataset/5.png");
+	resize(grey,grey,Size(dataWidth,dataHeight));
 	cvtColor(grey,grey,CV_BGR2HSV);
 	imshow("grey",grey);
-	setMouseCallback("grey",CallBackFunc,&grey);
+	setMouseCallback("grey",CallBackFunc,&grey);*/
 
-	Mat grey1 = imread("1.png");
-	cvtColor(grey1,grey1,CV_BGR2HSV);
-	imshow("grey1",grey1);
-	setMouseCallback("grey1",CallBackFunc,&grey1);
-	//imshow("image",image);
+	
+	//imshow("testImage",testImage);
 	waitKey(0);
 
 }
